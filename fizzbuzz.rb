df04c7116aa65341b391340a0e28f364828e4939
johnkounelias@gmail.com Kounelias <johnkounelias@gmail.com>
2018-06-06 17:18:30 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,14 @@
 def fizzbuzz(x)
+  
+if(x / 5 == 0 && x / 3 == 0)
+puts "FizzBuzz
 if (x / 3 == 0)
 puts "Fizz"
 
 elsif (x / 5 == 0)
 puts "Buzz"
 
-elsif(x / 5 == 0 && x / 3 == 0)
-puts "FizzBuzz"
+"
 
 else 
   puts x
