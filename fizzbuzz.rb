efdc4460066396ea192d117214a697439eaff296
johnkounelias@gmail.com Kounelias <johnkounelias@gmail.com>
2018-06-06 17:31:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 def fizzbuzz(x)
   
 if (x % 5 == 0 && x % 3 == 0)
-return "FizzBuzz"
+print "FizzBuzz"
 
 elsif (x %  3 == 0)
-return "Fizz"
+print "Fizz"
 
 elsif (x %  5 == 0)
-return "Buzz"
+print "Buzz"
 
 end
